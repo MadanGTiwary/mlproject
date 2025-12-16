e728073d252e2ce312e4db61806a2fa27e1db5dc
MadanGTiwary <madan.tiwary26@gmail.com>
2025-12-17 01:42:40 +0530

    model trainer and test

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 # class to save test data, train data, raw data etc.
 @dataclass
 class DataIngestionConfig:
@@ -55,7 +58,11 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj = DataIngestion()
+    # obj.initiate_data_ingestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
